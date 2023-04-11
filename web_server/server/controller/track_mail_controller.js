e97b7523aa01b432b5d1cda3b43e09fbadcc6f15
Tim <tingyilia00@gmail.com>
2023-04-11 14:49:40 +0800

    [fix]get the user-agent when user clicks on the  tracking URL

--- web_server/server/controller/track_mail_controller.js
+++ web_server/server/controller/track_mail_controller.js
@@ -13,6 +13,7 @@ export async function trackMail(req, res, next) {
   let origin = req.headers["origin"];
   console.log(referer);
   console.log(origin);
+  console.log(req.headers);
   if (checkImagePath == process.env.TRACKING_PIXEL_PATH) {
     // TODO:檢查發現進來的路徑是照片，那就可以查看是不是確實是我寄出的信件，是我寄出的信件就可以解雜湊emailId後存東西到資料庫說有被開信
     // console.log(req.headers);
