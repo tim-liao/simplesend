4dd7bb0befc0e9b5667db1e14f7011fa5f2b8dfe
Tim <tingyilia00@gmail.com>
2023-04-24 00:03:20 +0800

    [fix]debug

--- web_server/server/route/sns_route.js
+++ web_server/server/route/sns_route.js
@@ -1,6 +1,8 @@
 import express from "express";
 const router = express.Router();
+
+import bodyParser from "body-parser";
 import { wrapAsync, authentication } from "../../util/util.js";
 import { sns } from "../controller/sns_controller.js";
-router.route("/sns").post(wrapAsync(sns));
+router.route("/sns").post(bodyParser.text(), wrapAsync(sns));
 export default router;
